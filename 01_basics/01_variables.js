2054ed6fc8a39ff22d8e8b615a438dfc255303c1
Jiya Dave <jiyadave981@gmail.com>
2024-11-07 09:49:19 +0000

    control config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "jiya@google.com"
+var accountPassword="12345"
+accountCity = "Ahmedabad"
+
+// accountId = 2 //not allowed to change constant 
+// console.log(accountId);
+accountEmail="jd.com"
+accountPassword="21443242"
+accountCity="Delhi"
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+//diffrence const 
+//no use of var because issue in block scope and functional scope ,  hence use let for working anywhere  
