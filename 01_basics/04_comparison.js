db5e699c4279a20d4ff5a5e09d31afcca0aad9c7
Jiya Dave <jiyadave981@gmail.com>
2024-12-25 14:05:01 +0000

    comparison operations

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+//basic ones simple 
+// console.log("2">1);
+// console.log("02">1); //datatype should be same when comparing two values so as to avoid mistakes in case not predicted output is displayed
+
+// console.log(null >0); //false
+// console.log(null ==0);//false
+// console.log(null >= 0); //true    
+//>= works differently from > <. >= converts the null here to number 0 but > only converts to NaN can't be displayed predicted output
+//same goes for undefined too
+// console.log(undefined ==0); //false for all three 
+// console.log(undefined >0);
+// console.log(undefined <0);
+//THE ABOVE CONVERSION CAN LEAD YOU TO CONFUSION SO AVOID USING (JUST RUN FOR KNOWLEDGE PURPOSE)
+//=== strict check
+console.log("2"===2);
