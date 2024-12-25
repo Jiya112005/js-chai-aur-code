3916655947cb85e34326773533f67edeb4f1bb7d
Jiya Dave <jiyadave981@gmail.com>
2024-12-25 12:53:06 +0000

    code config

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+//old javascript does not contain classes modules etc advanced 
+"use strict"; //treat the code as newer version of js
+//alert(3+3)  different syntax to use alert in node than in browser 
+
+//when to use semicolons or not (explaining code readability)
+console.log(3+3); 
+
+console.log("Jiya")   //code should be readable it is not at all clearly visible 
+//summary : code readability is priority prettier extension for indentation can be used 
+//standards documentation ecma script and mdn 
+
+//datatypes not too much 
+let name="hitesh" //string
+let age = 18 //number type
+let isLoggedin = false //boolean
+//types: number(range is 2^53) 
+//bigint
+//string single or double quotes
+//boolean (ha ya na !!) true or false 
+
+//null it is standalone value (can be used to define variable) 
+//undefined value defined nai kari   
+let state;
+
+//symbol datatype => react component identification very useful in react for uniqueness
+
+//object 
+console.log(typeof null) //object type will be displayed and undefined 's type is undefined it is itself a type
+console.log(typeof undefined)
