0f5bb07088f95002edbd563247a604fe3710b088
Jiya Dave <jiyadave981@gmail.com>
2025-01-03 19:35:37 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,6 @@
+//for loop (basic)
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+// console.log(index); //not accessible due to local scope as we know
