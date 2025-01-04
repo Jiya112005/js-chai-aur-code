62d151cfacfe545620faccf3fef38d27fc567945
Jiya Dave <jiyadave981@gmail.com>
2025-01-04 09:19:39 +0000

    loops

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,6 +1,25 @@
 //for loop (basic)
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
     
-}
+// }
 // console.log(index); //not accessible due to local scope as we know
+
+//break and continue
+// for (let i = 1; i <=20; i++) {
+//     if(i==5){
+//         console.log(`Detected five`);
+//         break
+//     }
+//     console.log(`Value of i is: ${i}`);
+// }
+//break when five detected loop stopped is working also used in switch case control flow working 
+// for (let i = 1; i <=20; i++) {
+//     if(i==5){
+//         console.log(`Detected five`);
+//         continue
+//     }
+//     console.log(`Value of i is: ${i}`);
+// }
+//continue: ignore condition or iteration 
+
---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+//only condition checked and executed 
+//but has to mention initialization and termination and increments inside the loop
+
+// let i=0
+
+// while (i<=10) {
+//     console.log(`Value of index is : ${i}`);
+//     i=i+2    
+// }
+
+//while loop for arrays
+// const myarray = ["flash","batman","superman"]
+// let arrIndex = 0
+// while (arrIndex<myarray.length) {
+//     console.log(`Values is ${myarray[arrIndex]}`);
+//     arrIndex++
+// }
+let score=1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <=10);
+
+//first the statement executes and then loop is traversed later 
