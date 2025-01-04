206ef01a99d824d3b4cd673b41e4b0e58ce8b81a
Jiya Dave <jiyadave981@gmail.com>
2025-01-04 10:49:07 +0000

    adding the shopping cart example

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -16,4 +16,30 @@ const myNums = [1,2,3]
 const myTotal = myNums.reduce( (acc,currVal)=> acc+currVal ,0)
 
 
-console.log(myTotal);
+// console.log(myTotal);
+
+
+//shopping cart example
+const shoppingCart=[
+    {
+        itemName:'js course',
+        price:2999
+    },
+    {
+        itemName:'python course',
+        price:999
+    },
+    {
+        itemName:'mobile dev course',
+        price:5999
+    },
+    {
+        itemName:'data science course',
+        price:12999
+    }
+]
+
+//add to cart 
+const priceToPay= shoppingCart.reduce( (acc,item) => (acc + item.price),0) //for example it is done diretly otherwise for website first verify the type of data 
+console.log(priceToPay);
+
